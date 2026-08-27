57110500d4764592a697a56ac1c4d6c1893138b8
ilterdogac <44652584+ilterdogac@users.noreply.github.com>
2026-08-27 18:05:24 +0300

    Update hardware.py

--- utils/hardware.py
+++ utils/hardware.py
@@ -1,9 +1,11 @@
 import torch
 
-
+    # TODO: Maybe add xla support in future (not as easy as simply listing it here, as far as I remember)
 def get_device() -> torch.device:
     if torch.cuda.is_available():
         return torch.device("cuda")
     if torch.backends.mps.is_available():
         return torch.device("mps")
+    if torch.xpu.is_available():
+        return torch.device("xpu")
     return torch.device("cpu")
